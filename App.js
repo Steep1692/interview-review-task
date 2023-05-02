d9bd57979603a9d6e0640281f86f354766b2aea5
dmlen <dmlen@rollncode.com>
2023-05-02 16:00:42 +0300

    [NO STORY] Improve review experience.

--- App.js
+++ App.js
@@ -1,42 +1,24 @@
 import React, { useLayoutEffect } from "react";
-import styled from "styled-components";
 import Chat from "./components/Chat";
-import Auth from "./components/Auth";
+import { Auth } from "@components";
 import { useDispatch, useSelector } from "react-redux";
-import { setUserToStoreAction } from "./redux/actions";
+import { setUserToStoreAction } from "@actions";
 
 function App() {
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
   const user = useSelector((state) => state.app.user);
   const userFromStorage = localStorage.getItem("user");
 
   useLayoutEffect(() => {
-    const user = JSON.parse(userFromStorage);
+    const user = JSON.parse(userFromStorage)
     user && dispatch(setUserToStoreAction(user));
   }, []);
 
-  return (
-    <SC.App>
-      <SC.Container>
-        {userFromStorage || user?.name ? <Chat /> : <Auth />}
-      </SC.Container>
-    </SC.App>
-  );
-}
+  if (userFromStorage || user?.name) {
+    return <Chat />;
+  }
 
-const SC = {
-  App: styled.div`
-    height: 100vh;
-    padding: 20px 10px;
-    background-color: #b9d6f0;
-  `,
-  Container: styled.div`
-    max-width: 1200px;
-    margin: 0 auto;
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-  `,
-};
+  return <Auth />;
+}
 
 export default App;
